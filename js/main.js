fcc797c2fa520352573cf0f0e30d4e47160ab664
mecozma <mecozma@gmail.com>
2018-03-23 02:10:14 +0000

    Finished models section

--- js/main.js
+++ js/main.js
@@ -79,3 +79,47 @@ var Dog = Animal.extend({
 
 var dog = new Dog();
 dog.walk();
+
+
+
+//Vehicle miniproject
+
+//model
+var Vehicle = Backbone.Model.extend({
+	idAttribute: 'registrationNumber',
+	rootUrl: '/api/vehicles',
+	validate: function(attrs) {
+		if (!attrs.registrationNumber) {
+			return "Your vehicle needs a registration number!";
+		}
+	},
+	
+	start: function() {
+		console.log('Vehicle started.')
+	}
+	
+});
+
+var Car = Vehicle.extend({
+	initialize: function() {
+	console.log('Car with registration number' + this.get('registrationNumber') + 'started!');
+	}
+});
+
+var car = new Car({
+	registrationNumber: 'XLI887',
+	color: 'blue'
+});
+
+car.unset('registrationNumber');
+
+
+if(!car.isValid()) {
+	console.log(car.validationError);
+};
+
+car.set('registrationNumber', 'XLI887');
+
+car.start();
+
+
